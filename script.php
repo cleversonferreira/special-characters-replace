4711be76b1f91220fd0f07d72f196af32ad563b9
Cleverson Ferreira <cleverson_reisferreira@hotmail.com>
2018-06-21 15:06:33 -0300

    Create script.php

---
+++ script.php
@@ -0,0 +1,165 @@
+<?php
+
+$file = 'your-file.html';
+
+// special characters array
+$data = array(
+	array(
+		"À",
+		"&Agrave;"
+	),
+	array(
+		"Á",
+		"&Aacute;"
+	),
+	array(
+		"Â",
+		"&Acirc;"
+	),
+	array(
+		"Ã",
+		"&Atilde;"
+	),
+	array(
+		"Å",
+		"&Aring;"
+	),
+	array(
+		"Ç",
+		"&Ccedil;"
+	),
+	array(
+		"È",
+		"&Egrave;"
+	),
+	array(
+		"É",
+		"&Eacute;"
+	),
+	array(
+		"Ê",
+		"&Ecirc;"
+	),
+	array(
+		"Í",
+		"&Iacute;"
+	),
+	array(
+		"Ò",
+		"&Ograve;"
+	),
+	array(
+		"Ó",
+		"&Oacute;"
+	),
+	array(
+		"Ô",
+		"&Ocirc;"
+	),
+	array(
+		"Õ",
+		"&Otilde;"
+	),
+	array(
+		"Ù",
+		"&Ugrave;;"
+	),
+	array(
+		"Ú",
+		"&Uacute;"
+	),
+	array(
+		"Û",
+		"&Ucirc;"
+	),
+	array(
+		"à",
+		"&agrave;"
+	),
+	array(
+		"á",
+		"&aacute;"
+	),
+	array(
+		"â",
+		"&acirc;"
+	),
+	array(
+		"ã",
+		"&atilde;"
+	),
+	array(
+		"ç",
+		"&ccedil;"
+	),
+	array(
+		"è",
+		"&egrave;"
+	),
+	array(
+		"é",
+		"&eacute;"
+	),
+	array(
+		"ê",
+		"&ecirc;"
+	),
+	array(
+		"ì",
+		"&igrave;"
+	),
+	array(
+		"í",
+		"&iacute;"
+	),
+	array(
+		"ñ",
+		"&ntilde;"
+	),
+	array(
+		"ò",
+		"&ograve;"
+	),
+	array(
+		"ó",
+		"&oacute;"
+	),
+	array(
+		"ô",
+		"&ocirc;"
+	),
+	array(
+		"õ",
+		"&otilde;"
+	),
+	array(
+		"ù",
+		"&ugrave;"
+	),
+	array(
+		"ú",
+		"&uacute;"
+	),
+	array(
+		"û",
+		"&ucirc;"
+	)
+);
+
+echo 'starting loop <br>';
+
+//for each position of the array replaces incidents
+foreach($data as $key => $value){
+
+	echo 'opening file <br>';
+
+	//pegar arquivo
+	$content = file_get_contents($file);
+
+	echo 'replacing - (' . $value[0] . ')<br>';
+
+	$new_content = str_replace($value[1], $value[0], $content);
+	
+	//save updated file
+	file_put_contents($file, $new_content);
+}
